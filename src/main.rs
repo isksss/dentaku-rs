73e9e30a7471802c44d68fa15736fb638e62e5b2
isksss <104404522+isksss@users.noreply.github.com>
2024-11-04 12:47:07 +0900

    5-2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::io::stdin;
+
 fn main() {
-    println!("Hello, world!");
+    for line in stdin().lines() {
+        // 空行なら終了
+        let line = line.unwrap();
+        if line.is_empty() {
+            break;
+        }
+
+        // 空白で分割
+        let tokens: Vec<&str> = line.split_whitespace().collect();
+
+        // 式の計算
+        let left: f64 = tokens[0].parse().unwrap();
+        let right: f64 = tokens[2].parse().unwrap();
+        let result = match tokens[1] {
+            "+" => left + right,
+            "-" => left - right,
+            "*" => left * right,
+            "/" => left / right,
+            _ => unreachable!(),
+        };
+
+        // 結果の表示
+        print_value(result);
+    }
+}
+
+fn print_value(value: f64) {
+    println!(" -> {}", value);
 }
